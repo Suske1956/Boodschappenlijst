8ddc9cfd53776f399ac6411373b26c966b625045
frans <fransvangenesen@gmail.com>
2021-06-17 09:41:04 +0200

    update

---
+++ Notes.md
@@ -0,0 +1,35 @@
+# Notes regarding shopping list  
+Database = sqlite since goal is "learn to work wit sqlite"  
+interface = cli since developing an interface is not a goal
+## Database operations    
+Chosen for:  "try: except: finally:" for errors in sql commands are not  
+easy to find. "except sqlite3.Error as error:" exports the error from  
+sqlite to the variable error
+## Considerations database structure  
+Table products with fields:
+- ID - int (use standard rowid sqlite)
+- product_name - text 
+- product_unit - text (pcs or kg)
+- number_required - real (how many items to purchase)
+- shop_id - int
+
+Table shops with fields: 
+- ID - int (use standard rowid sqlite)
+- shop_name - text
+
+The field products-product_unit is text. for the time being the program fills the field to keep it consistent.  
+The table with shops exists to keep everything consistent and easily change shop names
+## Considerations operations.  
+For the shopping list the operations are:  
+- create table(s): to be executed once, starting the database or in case of refreshing the entire content
+- add shop
+- remove shop
+- add item
+- change item
+- remove item
+- change requirement
+- reset all requirements
+- generate shopping list 
+## Class structure
+### Database operations
+
--- boodschappenlijst.py
+++ boodschappenlijst.py
@@ -1,5 +1,39 @@
 import sqlite3
 
+class DatabaseOperations:
+    def __init__(self):
+        self.db_name = 'boodschappenlijst.db'
+        self.q_create_table = '''CREATE TABLE SqliteDb_developers (
+                                        id INTEGER PRIMARY KEY,
+                                        name TEXT NOT NULL,
+                                        email text NOT NULL UNIQUE,
+                                        joining_date datetime,
+                                        salary REAL NOT NULL);'''
+        self.q_sqlite_version = "select sqlite_version();"
+
+    def db_operation(self):
+        try:
+            conn = sqlite3.connect(self.db_name)
+            c = conn.cursor()
+            print("Successfully Connected to SQLite")
+
+            c.execute(self.q_sqlite_version)
+            conn.commit()
+            record = c.fetchall()
+            print("SQLite Database Version is: ", record)
+
+            c.close()
+
+        except sqlite3.Error as error:
+            print("Error while creating a sqlite table", error)
+        finally:
+            if conn:
+                conn.close()
+                print("sqlite connection is closed")
+
+
+
+
 
 class Database:
     def __init__(self):
@@ -30,10 +64,10 @@ class Database:
         print(c.fetchall())
         conn.close()
 
+operations = DatabaseOperations()
+operations.db_operation()
 
-
-
-db = Database()
-db.create_table('test', ('een text', 'twee text'))
-db.add_record('test', ('afja', 'jhsdakfha'))
-db.drop_table('test')
+# db = Database()
+# db.create_table('test', ('een text', 'twee text'))
+# db.add_record('test', ('afja', 'jhsdakfha'))
+# db.drop_table('test')
