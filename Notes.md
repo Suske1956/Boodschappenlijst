9febbe725bcffea36006ae6d7c9e04b5d59b7670
frans <fransvangenesen@gmail.com>
2021-06-22 08:30:04 +0200

    Update

--- Notes.md
+++ Notes.md
@@ -9,7 +9,7 @@ sqlite to the variable error
 Table products with fields:
 - ID - int (use standard rowid sqlite)
 - product_name - text 
-- product_unit - text (pcs or kg)
+- product_unit - text (pcs kg, gram)
 - number_required - real (how many items to purchase)
 - shop_id - int
 
@@ -17,11 +17,18 @@ Table shops with fields:
 - ID - int (use standard rowid sqlite)
 - shop_name - text
 
-The field products-product_unit is text. for the time being the program fills the field to keep it consistent.  
+Table product_shop with fields:  
+- prod_id - int
+- shop_id - int
+- price - real
+
+The field products-product_unit is text. for the time being the program fills the field to keep 
+it consistent.  
 The table with shops exists to keep everything consistent and easily change shop names
 ## Considerations operations.  
 For the shopping list the operations are:  
-- create table(s): to be executed once, starting the database or in case of refreshing the entire content
+- create table(s): to be executed once, starting the database or in case of refreshing the 
+  entire content
 - add shop
 - remove shop
 - add item
@@ -32,12 +39,16 @@ For the shopping list the operations are:
 - generate shopping list 
 ## Class structure
 ### Menu  
-The menu will be as simple as possible. Obviously there will be a main menu with items like maintenance:  
+The menu will be as simple as possible. Obviously there will be a main menu with items 
+like maintenance:  
 for instance list and create tables to be decided: whether database operations get their own (sub)menu.  
-Giving each class its own menu makes it possible to confine the classes. We will get the following classes:  
+Giving each class its own menu makes it possible to confine operatins within classes.  
+We will get the following classes:  
 - Database operations: list tables, reset database
 - Maintenance: add, change and remove shops or items
-- Shopping list: list of items; reset requirements; add and remove requirements; generate shopping list
+- Shopping list: list of items; reset requirements; add and remove requirements; generate shopping list  
+Notes regarding menu's:  
+- I did not find an easy solution for "press any key", so I used "Press ENTER" instead.
 ### Database operations
 ### Maintenance
 ### Shopping list
--- boodschappenlijst.py
+++ boodschappenlijst.py
@@ -1,46 +1,78 @@
 import sqlite3
 import sys
 
+
 class DatabaseOperations:
     def __init__(self):
         self.db_name = 'boodschappenlijst.db'
-        self.q_create_table = '''CREATE TABLE SqliteDb_developers (
-                                        id INTEGER PRIMARY KEY,
-                                        name TEXT NOT NULL,
-                                        email text NOT NULL UNIQUE,
-                                        joining_date datetime,
-                                        salary REAL NOT NULL);'''
+        # currently queries without parameters (this class is probably used once).
+        # parameters to be added later on
+        self.q_create_table_products = '''CREATE TABLE products (
+                                prod_name TEXT NOT NULL,
+                                prod_unit text NOT NULL,
+                                prod_required REAL,
+                                shop_id INTEGER);'''
+        self.q_create_table_shops = '''CREATE TABLE shops (
+                                shop_name TEXT);'''
+        self.q_create_table_prod_shop = '''CREATE TABLE prod_shop (
+                                prod_id INTEGER,
+                                shop_id INTEGER,
+                                prod_price REAL);'''
+        self.q_drop_table_products = "DROP TABLE products"
+        self.q_drop_table_shops = "DROP TABLE shops"
+        self.q_drop_table_prod_shop = "DROP TABLE prod_shop"
+
+    def menu(self):
+        while True:
+            print('\n    Tabellen menu, maak je keuze \n1:  Lijst tabellen \n2:  Tabellen verversen \n9:  Terug')
+            keuze = input()
+            if keuze == '1':
+                print('1 gekozen')
+                self.list_tables()
+            elif keuze == '2':
+                self.reset_database()
+            elif keuze == '9':
+                break
+            else:
+                print('Ongeldige keuze')
 
     def list_tables(self):
+        conn = None
         try:
             conn = sqlite3.connect(self.db_name)
             c = conn.cursor()
-            print("Successfully Connected to: " + self.db_name)
             c.execute("SELECT name FROM sqlite_master WHERE type='table';")
             conn.commit()
-            print(c.fetchall())
+            tables = (c.fetchall())
+            print('\nList of tables:')
+            for item in tables:
+                print(item[0])
+            print('Druk op ENTER om door te gaan')
+            input()
             c.close()
         except sqlite3.Error as error:
             print("Error while listing tables", error)
         finally:
             if conn:
                 conn.close()
-                print("sqlite connection is closed")
+
+    def reset_database(self):
+        print('ATTENTION:\n After resetting the database all data will be lost \nDo you want to continue? (yes/No\n')
+        answer = input()
+        print(answer.upper())
+        if answer.upper() == 'YES':
+            print('reset')
+        else:
+            print('aborted')
 
     # create table, arguments: table name (string) and column list (tuple of strings)
     def create_table(self):
+        conn = None
         try:
             conn = sqlite3.connect(self.db_name)
-            sqlite_create_table_query = '''CREATE TABLE SqliteDb_developers (
-                                        id INTEGER PRIMARY KEY,
-                                        name TEXT NOT NULL,
-                                        email text NOT NULL UNIQUE,
-                                        joining_date datetime,
-                                        salary REAL NOT NULL);'''
-
             c = conn.cursor()
             print("Successfully Connected to SQLite")
-            c.execute(sqlite_create_table_query)
+            c.execute(self.q_create_table_prod_shop)
             conn.commit()
             print("SQLite table created")
 
@@ -55,30 +87,30 @@ class DatabaseOperations:
 
     # delete table, arguments: table name (string)
     def drop_table(self):
+        conn = None
         try:
             conn = sqlite3.connect(self.db_name)
-            sqlite_drop_table_query = "DROP TABLE SqliteDb_developers"
             c = conn.cursor()
-            c.execute(sqlite_drop_table_query)
+            c.execute(self.q_drop_table_prod_shop)
             conn.commit()
-            print('table successfully removed0')
+            print('table successfully removed')
             c.close()
         except sqlite3.Error as error:
-            print("Error while creating a sqlite table", error)
+            print('Error while removing a table', error)
         finally:
             if conn:
                 conn.close()
-                print("sqlite connection is closed")
-
+                print('sqlite connection is closed')
 
 
 # menu, note no clear screen since is is, afaik, not platform independent
 def menu():
     while True:
-        print('Welkom maak je keuze\n1:  Database\n2:  Onderhoud producten\n3:  Boodschappenlijst\n9: verlaten ')
+        print('\n    Hoofdmenu maak je keuze\n1:  Tabellen\n2:  Onderhoud producten\n3:  '
+              'Boodschappenlijst\n9: verlaten ')
         keuze = input()
         if keuze == '1':
-            print('een gekozen')
+            operations.menu()
         elif keuze == '2':
             print('2 gekozen')
         elif keuze == '3':
@@ -90,9 +122,9 @@ def menu():
 
 
 operations = DatabaseOperations()
-# menu()
-operations.list_tables()
+menu()
+# operations.list_tables()
 # operations.create_table()
-operations.list_tables()
+# operations.list_tables()
 # operations.drop_table()
-operations.list_tables()
+# operations.list_tables()
