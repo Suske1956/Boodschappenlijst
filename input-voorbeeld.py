18e2ed5a2c7df1862c1f9d01eb757f78e9ac879f
Suske1956 <83999682+Suske1956@users.noreply.github.com>
2021-07-11 08:47:25 +0200

    Delete input-voorbeeld.py

--- input-voorbeeld.py
+++
@@ -1,8 +0,0 @@
-# even wat geprobeerd met input. Dit kan later wel weg
-
-print('wat is je naam? ')
-txt = 'geef een getal'
-x = int(input(txt))
-print(x)
-print(type(x))
-# je krijgt altijd een string terug
